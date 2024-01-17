865cf1df8c5a8c8c07ca373580ee353c0690c02b
unknown <ayushcodingprofile@gmail.com>
2024-01-17 14:46:43 +0530

    props tailwind

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj ={
+    city:"ABC",
+    state:"XYZ",
+    country:"PQR",
+  }
 
   return (
     <>
-      <Card/>
+      <Card username="ayush" objectSample={myObj}/>
       <h1 className=' text-3xl bg-green-300'>Vite With Tailwind</h1>
     </>
   )
