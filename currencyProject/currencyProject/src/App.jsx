a2a521334d2cb23eb5b802ea811bc17cebd15185
unknown <ayushcodingprofile@gmail.com>
2024-01-23 21:22:23 +0530

    input boxes

---
+++ currencyProject/currencyProject/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center
+    items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://images.pexels.com/photos/8370752/pexels-photo-8370752.jpeg)`,
+      }}
+    >
+      
+    </div>
+  );
+}
+
+export default App
