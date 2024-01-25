afc3eb17dd992e35c7ddc475afdb14451efabfe9
unknown <ayushcodingprofile@gmail.com>
2024-01-25 15:39:19 +0530

    Routing Files Setup

---
+++ routingProject/src/components/index.jsx
@@ -0,0 +1,7 @@
+import Home from "./Home.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Contact from "./Contact.jsx";
+import About from "./AboutUs.jsx";
+
+export default {Home, Header, Footer, Contact, About};
